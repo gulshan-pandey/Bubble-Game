a41b435d2ac7c0d9049ddb8877deebfd4bdb8580
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-05-16 16:07:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let Time=3
+let Time=31
 var score = 0;
 
 function HitRandom(){
@@ -27,9 +27,20 @@ function runtime(){
         }
         else{
 
-            document.querySelector("#bottom").innerHTML= `<h1>Game Over and Your score is ${score}</h1> `          // appears when game is over
-            
             clearInterval(timer)
+            document.querySelector("#bottom").innerHTML= `<h1>Game Over and Your score is ${score}</h1> `          // appears when game is over
+            let but = document.createElement("button");
+            but.textContent = "Play Again"; // Use textContent for text
+            but.style.cssText = "background-color: rgb(59, 42, 145); color: white; border: none; border-radius: 0.5rem; padding: 0.5rem 0.6rem; text-align: center; text-decoration: none; display: inline-block; font-size: 1.2rem; margin: 0.5rem 0.1rem; cursor: pointer;";
+           
+
+            but.addEventListener("click", function() {
+                this.style.backgroundColor = "rgb(82, 55, 216)"; 
+                location.reload();
+            });
+
+            // Append the button to the bottom container
+            document.querySelector("#bottom").appendChild(but);
         }
     },1000)
 
